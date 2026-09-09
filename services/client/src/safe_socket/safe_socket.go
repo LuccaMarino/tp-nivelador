50cf7b0112063eb658882ba2048318f11c7790e1
LuccaMarino <chinimarino@gmail.com>
2026-09-09 01:22:26 -0300

    ejercicio 4

--- services/client/src/safe_socket/safe_socket.go
+++ services/client/src/safe_socket/safe_socket.go
@@ -2,21 +2,31 @@ package safe_socket
 
 import "io"
 
-//TODO: Complete with a short-read/short-write tolerant implementation
-
 func SendAll(socket io.Writer, bytes []byte) error {
-	_, err := socket.Write(bytes)
-	if err != nil {
-		return err
+	for len(bytes) > 0 {
+		n, err := socket.Write(bytes)
+		if err != nil {
+			return err
+		}
+		bytes = bytes[n:] // muevo el slice en n posiciones para enviar los bytes restantes
 	}
 	return nil
 }
 
 func RecvAll(socket io.Reader, size int) ([]byte, error) {
 	buff := make([]byte, size)
-	n, err := socket.Read(buff)
-	if err != nil {
-		return nil, err
+	total := 0
+	for total < size {
+		n, err := socket.Read(buff[total:])
+		total += n
+		if total == size { // si ya se leyeron todos los bytes esperados salgo del loop
+			break
+		}
+		// manejo el err despues de procesar los bytes leidos,
+		// lo recomienda la documentacion de io.Reader
+		if err != nil {
+			return nil, err
+		}
 	}
-	return buff[:n], nil
+	return buff, nil
 }
--- services/server/src/safe_socket/safe_socket.py
+++ services/server/src/safe_socket/safe_socket.py
@@ -1,11 +1,21 @@
 import socket
 
-# TODO: Complete with a short-read/short-write tolerant implementation
 
 
 def recv_all(socket: socket.socket, size):
-    return socket.recv(size)
+    data = b""
+    while len(data) < size:
+        chunk = socket.recv(size - len(data))
+        if not chunk:
+            if not data:
+                return b""
+            raise ConnectionError("[recv] connection closed before receiving all data")
+        data += chunk
+    return data
 
 
 def send_all(socket: socket.socket, bytes):
-    return socket.send(bytes)
+    total_sent = 0
+    while total_sent < len(bytes):
+        sent = socket.send(bytes[total_sent:])
+        total_sent += sent
